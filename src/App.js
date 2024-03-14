6d46b00c5b158b7c9d86941314c8b3824683963b
Willian <wolverinewa@gmail.com>
2024-03-14 10:05:05 -0300

    Adicionado fontAwesome

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ import './App.css';
 let initialState = [
   {
     id: 1,
-    descricao: "Primeira Atividade"
+    descricao: "Primeira Atividade",
+    prioridade: "Baixa",
+    titulo: "Atividade"
   },
   {
     id: 2,
-    descricao: "Segunda Atividade"
-  },
-  {
-    id: 3,
-    descricao: "Terceira Atividade"
-  },
-  {
-    id: 4,
-    descricao: "Quarta Atividade"
+    descricao: "Segunda Atividade",
+    prioridade: "Baixa",
+    titulo: "Atividade"
   },
 ];
 
@@ -30,6 +26,8 @@ function App() {
     const atividade = {
       id: document.getElementById('id').value,
       descricao: document.getElementById('descricao').value,
+      titulo: document.getElementById('titulo').value,
+      prioridade: document.getElementById('prioridade').value,
     };
 
     setAtividades([...atividades, { ...atividade }]);
@@ -39,14 +37,32 @@ function App() {
     <>
       <form className="row g-3">
         <div className="col-md-6">
-          <label htmlFor="inputid" className="form-label">ID</label>
+          <label className="form-label">ID</label>
           <input id="id" type="text" className="form-control" />
         </div>
+
         <div className="col-md-6">
-          <label htmlFor="inputdescricao" className="form-label">Descrição</label>
+          <label className="form-label">Prioridade</label>
+          <select id="prioridade" className="form-select">
+            <option defaultValue="0">Selecionar...</option>
+            <option value="1">Baixa</option>
+            <option value="2">Normal</option>
+            <option value="3">Alta</option>
+          </select>
+        </div>
+
+        <div className="col-md-6">
+          <label className="form-label">Título</label>
+          <input id="titulo" type="text" className="form-control" />
+        </div>
+
+        <div className="col-md-6">
+          <label className="form-label">Descrição</label>
           <input id="descricao" type="text" className="form-control" />
         </div>
+
         <hr />
+
         <div className="col-12">
           <button className="btn btn-outline-secondary" onClick={addAtividade}>+ Atividade</button>
         </div>
@@ -59,12 +75,31 @@ function App() {
               <div className='card-body'>
                 <div className="d-flex justify-content-between">
                   <h5 className="cadr-title">
-                    <span className="badge rounded-pill bg-primary"> Primary</span>
-                    {ativ.id} - Titulo
+                    <span className="badge bg-secondary me-1">{ativ.id}</span>
+                    - {ativ.titulo}
                   </h5>
-                  <h6>Prioridade: Normal</h6>
+                  <h6>
+                    Prioridade:
+                    <span className="ms-1 text-black">
+                      <i className="me-1 far fa-frown-open"></i>
+                      {ativ.prioridade}
+                    </span>
+                  </h6>
                 </div>
+
                 <p className='card-text'>{ativ.descricao}</p>
+
+                <div className="d-flex justify-content-end pt-2 m-0 border-top">
+                  <button className='btn btn-sm btn-outline-primary me-2'>
+                    <i className='fa fa-pen me-2'></i>
+                    Editar
+                  </button>
+
+                  <button className='btn btn-outline-danger'>
+                    <i className='fa fa-trash-o me-2'></i>
+                    Deletar
+                  </button>
+                </div>
               </div>
             </div>
           )
